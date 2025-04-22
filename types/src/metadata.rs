5880a6a6e4016edb2c7f5016762cbb05da3456a1
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2025-04-22 18:00:18 +0900

    remove precompile metadata from sdk

--- types/src/metadata.rs
+++ types/src/metadata.rs
@@ -31,32 +31,3 @@ pub struct PodLogMetadata {
     pub attestations: Vec<TimestampedHeadlessAttestation>,
     pub receipt: Receipt,
 }
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct PrecompileMetadata {
-    pub chain_id_to_rpc_url: HashMap<u64, String>,
-}
-
-impl PrecompileMetadata {
-    pub fn new() -> Self {
-        Self {
-            chain_id_to_rpc_url: HashMap::new(),
-        }
-    }
-
-    pub fn add_chain(&mut self, chain_id: u64, rpc_url: String) {
-        self.chain_id_to_rpc_url.insert(chain_id, rpc_url);
-    }
-
-    pub fn get_rpc_url(&self, chain_id: u64) -> Option<&String> {
-        self.chain_id_to_rpc_url.get(&chain_id)
-    }
-}
-
-impl Default for PrecompileMetadata {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-pub static PRECOMPILE_METADATA_SINGLETON: OnceLock<PrecompileMetadata> = OnceLock::new();
