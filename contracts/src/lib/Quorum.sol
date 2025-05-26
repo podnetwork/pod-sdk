7c68a2e3fa27182b2ed92f217e378ca4a7818b9e
Alex Brezas <abresas@gmail.com>
2025-05-26 19:45:37 +0300

    make requireQuorum testable

--- contracts/src/lib/Quorum.sol
+++ contracts/src/lib/Quorum.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.8.28;
 
 address constant REQUIRE_QUORUM = address(uint160(uint256(keccak256("POD_REQUIRE_QUORUM"))));
 
-function requireQuorum(bool input, string memory _message) view {
-    REQUIRE_QUORUM.staticcall(abi.encode(input));
+function requireQuorum(bool input, string memory message) view {
+    (bool success, bytes memory _data) = REQUIRE_QUORUM.staticcall(abi.encode(input));
+    // assumes the only way to fail is if the quorum is not met
+    // in fact in production the contract call will revert before this require is even executed
+    // and if the quorum is met it will always succeed
+    // however this check is required for testing purposes
+    require(success, message);
 }
