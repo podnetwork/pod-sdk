05b3f123d165de2b60c70621f40c7c3a6b1a9b07
Bartosz Różański <bartek.roza@gmail.com>
2025-06-25 12:39:08 +0200

    PoC optimism TX auction

---
+++ examples/optimism-tx-auction/submit_txs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <destination_address> --to <to_address> --amount <amount>"
+    exit 1
+fi
+DESTINATION_ADDRESS=$1
+TO_ADDRESS=""
+AMOUNT=""
+
+while [[ "$#" -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --to)
+      TO_ADDRESS="$2"
+      shift
+      shift
+      ;;
+    --amount)
+      AMOUNT="$2"
+      shift
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if [[ -z "$TO_ADDRESS" || -z "$AMOUNT" ]]; then
+    echo "Both --to and --amount arguments must be provided"
+    exit 1
+fi
+
+
+declare -a PRIVATE_KEYS=(
+    "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba"
+    "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"
+    "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356"
+    "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97"
+    "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
+)
+# Loop through each private key and submit a transaction
+for PRIVATE_KEY in "${PRIVATE_KEYS[@]}"; do
+    echo "Submitting transaction with private key: $PRIVATE_KEY to destination: $DESTINATION_ADDRESS"
+
+    if ! cargo run --bin send_tx -- \
+	--private-key "$PRIVATE_KEY" \
+	--to "$TO_ADDRESS" \
+	--amount "$AMOUNT"; then
+	echo "Failed to submit transaction with private key: $PRIVATE_KEY"
+	exit 1
+    fi
+    echo "Transaction submitted successfully with private key: $PRIVATE_KEY"
+done
+
+echo "All transactions submitted successfully."
+
+
