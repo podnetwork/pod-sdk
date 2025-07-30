eef100114327a875fd443b28f0068d644ed1615c
João Azevedo <joaoazevedo@ieee.org>
2025-07-30 12:18:40 +0200

    Merge branch 'main' into feat/timestamp-precompile

--- examples/voting/contract/test/Voting.t.sol
+++ examples/voting/contract/test/Voting.t.sol
@@ -31,73 +31,94 @@ contract VotingTest is PodTest {
         return users;
     }
 
-    function test_createPoll() public {
+    function test_createProposal() public {
         vm.expectEmit(false, true, false, true);
-        emit Voting.PollCreated(bytes32(0), Time.currentTime().addSeconds(1));
+        emit Voting.ProposalCreated(bytes32(0), Time.currentTime().addSeconds(1), "test");
 
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, createUsers(1));
-        console.log("created poll with id:");
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, createUsers(1), "test");
+        console.log("created proposal with id:");
         console.logBytes32(id);
     }
 
     function test_mustBeParticipantToVote() public {
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, createUsers(10));
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, createUsers(10), "test");
         vm.prank(vm.addr(999));
-        vm.expectRevert("sender can't vote");
-        voting.vote(id, 1);
-    }
-
-    function test_checksChoiceBounds() public {
-        address[] memory users = createUsers(10);
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, users);
-
-        vm.startPrank(users[0]);
-        vm.expectRevert("Choice must be between 1 and maxChoice");
-        voting.vote(id, 0);
-        vm.expectRevert("Choice must be between 1 and maxChoice");
-        voting.vote(id, 4);
+        vm.expectRevert("sender not a voter");
+        voting.castVote(id, 1);
     }
 
     function test_cantVoteTwice() public {
         address[] memory users = createUsers(10);
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, users);
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, users, "test");
 
-        vm.startPrank(users[0]);
-        voting.vote(id, 1);
-        vm.expectRevert("sender can't vote");
-        voting.vote(id, 1);
+        vm.startBroadcast(users[0]);
+        voting.castVote(id, 1);
+        vm.expectRevert("already voted");
+        voting.castVote(id, 1);
     }
 
-    function test_cantCreatePollAfterDeadline() public {
+    function test_cantCreateProposalAfterDeadline() public {
         Time.Timestamp currentTime = Time.currentTime();
         address[] memory users = createUsers(10);
 
         vm.expectRevert("Deadline must be in the future");
-        voting.createPoll(currentTime, 3, users);
+        voting.createProposal(currentTime, 3, users, "test");
     }
 
     function test_cantVoteAfterDeadline() public {
         address[] memory users = createUsers(10);
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, users);
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, users, "test");
 
         podWarp(Time.currentTime().addSeconds(1));
         vm.startPrank(users[0]);
-        vm.expectRevert("Poll deadline has passed or poll does not exist");
-        voting.vote(id, 1);
+        vm.expectRevert("Proposal deadline has passed or proposal does not exist");
+        voting.castVote(id, 1);
     }
 
-    function test_singleVoterPoll() public {
+    function test_singleVoterProposal() public {
         address[] memory users = createUsers(1);
-        bytes32 id = voting.createPoll(Time.currentTime().addSeconds(1), 3, users);
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, users, "test");
 
-        vm.startPrank(users[0]);
+        vm.startBroadcast(users[0]);
         vm.expectEmit();
-        emit Voting.Voted(id, users[0], 1);
-        voting.vote(id, 1);
+        emit Voting.VoteCast(id, users[0], 1);
+        voting.castVote(id, 1);
 
         podWarp(Time.currentTime().addSeconds(2));
         vm.expectEmit(true, true, false, true);
-        emit Voting.Winner(id, 1);
-        voting.setWinningChoice(id, 1);
+        emit Voting.ProposalExecuted(id);
+        voting.execute(id);
+    }
+
+    function test_voteAndPickWinner() public {
+        address[] memory users = createUsers(10);
+        bytes32 id = voting.createProposal(Time.currentTime().addSeconds(1), 3, users, "test");
+
+        for (uint256 i = 0; i < users.length; i++) {
+            vm.startBroadcast(users[i]);
+            vm.expectEmit();
+            emit Voting.VoteCast(id, users[i], 1);
+
+            voting.castVote(id, 1);
+            vm.stopBroadcast();
+        }
+
+        vm.startBroadcast(users[0]);
+
+        // before deadline
+        vm.expectRevert("Proposal deadline has not passed yet");
+        voting.execute(id);
+
+        // after deadline
+        podWarp(Time.currentTime().addSeconds(2));
+
+        // success
+        vm.expectEmit(true, true, false, true);
+        emit Voting.ProposalExecuted(id);
+        voting.execute(id);
+
+        // can't select execute again
+        vm.expectRevert("Proposal already executed");
+        voting.execute(id);
     }
 }
