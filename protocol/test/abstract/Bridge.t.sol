9dfd73a38a2ab2c649672b6d5e276c973b71eca0
Jakov Mitrovski <jakovvmitrovski@yahoo.com>
2025-09-04 16:50:23 +0200

    fix typo

--- protocol/test/abstract/Bridge.t.sol
+++ protocol/test/abstract/Bridge.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {PodTest} from "pod-sdk/test/PodTest.sol";
+import {PodTest} from "pod-sdk/test/podTest.sol";
 import {Bridge} from "../../src/abstract/Bridge.sol";
 import {IBridge} from "../../src/interfaces/IBridge.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
