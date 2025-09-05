5a8f8bb08e00c7cd3c5c639f80479c1cf1cbd351
Bartosz Różański <bartek.roza@gmail.com>
2025-09-05 09:03:57 +0200

    TestBridgePrecompile test contract (#124)

--- examples/solidity/script/Deploy.s.sol
+++ examples/solidity/script/Deploy.s.sol
@@ -9,6 +9,7 @@ import {Voting} from "../src/Voting.sol";
 import {BaseDeployer} from "pod-protocol-scripts/BaseDeployer.s.sol";
 import {PodRegistry} from "pod-protocol/PodRegistry.sol";
 import {PodAuctionConsumer} from "optimistic-auction/PodAuctionConsumer.sol";
+import {TestMintBalancePrecompile} from "../src/TestMintBalancePrecompile.sol";
 
 contract Deployer is BaseDeployer {
     function run() public {
@@ -37,6 +38,9 @@ contract Deployer is BaseDeployer {
             PodAuctionConsumer auctionConsumer = new PodAuctionConsumer(address(podRegistry), bondAmount);
 
             console.log("PodAuctionConsumer deployed at:", address(auctionConsumer));
+
+            TestMintBalancePrecompile testMintBalance = new TestMintBalancePrecompile();
+            console.log("TestMintBalancePrecompile contract deployed at:", address(testMintBalance));
         }
 
         vm.stopBroadcast();
