301fd6bf423636505b1dc483726b68a54cfd2a6f
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2025-09-10 14:29:47 +0300

    arbitrary attestation metadata

--- types/src/consensus/attestation_metadata.rs
+++ types/src/consensus/attestation_metadata.rs
@@ -3,6 +3,7 @@ use alloy_primitives::Address;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
 pub struct AttestationMetadata {
     pub timestamp: Timestamp,
     pub global_sequence: u64,
