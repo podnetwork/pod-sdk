de256f0a654414e995a95df99d0bcaa2d2032448
anlithov <anlithov@gmail.com>
2025-10-27 17:03:09 +0200

    fix: Deploy script

--- protocol/script/deploy_bridges.bash
+++ protocol/script/deploy_bridges.bash
@@ -31,13 +31,13 @@ echo "USER_ADDRESS: $USER_ADDRESS"
 : "${MIRROR_SYMBOL:=pwTEST}"
 : "${MIRROR_TOKEN_NAME:=Pod Wrapped Test Token}"
 : "${DECIMALS:=18}"
-: "${MINT_AMOUNT:=100000000000000000000}"       # 10e18
+: "${MINT_AMOUNT:=10000000000000}"       # 10e18
 
 # Limits
-: "${NATIVE_MIN:=10000000000000000}"            # 0.01 ether
+: "${NATIVE_MIN:=1000000000}"            
 : "${NATIVE_DEP:=500000000000000000000}"        # 500e18
 : "${NATIVE_CLA:=500000000000000000000}"        # 500e18
-: "${TOKEN_MIN:=10000000000000000}"             # 0.01e18
+: "${TOKEN_MIN:=1000000000}"
 : "${TOKEN_DEP:=1000000000000000000000}"        # 100e18
 : "${TOKEN_CLA:=1000000000000000000000}"        # 100e18
 
@@ -67,8 +67,15 @@ SOURCE_CHAIN_DEPLOYER=$(cast wallet address --private-key "$PK_SOURCE_CHAIN")
 POD_DEPLOYER=$(cast wallet address --private-key "$PK_POD")
 SOURCE_CHAIN_NONCE=$(cast nonce "$SOURCE_CHAIN_DEPLOYER" --rpc-url "$SOURCE_CHAIN_RPC")
 POD_NONCE=$(cast nonce "$POD_DEPLOYER" --rpc-url "$POD_RPC")
-SOURCE_CHAIN_BRIDGE_ADDR=$(cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 3))" |  grep -oE '0x[0-9a-fA-F]{40}')
+SOURCE_CHAIN_BRIDGE_ADDR=$(cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 1))" |  grep -oE '0x[0-9a-fA-F]{40}')
 POD_BRIDGE_ADDR=$(cast compute-address "$POD_DEPLOYER" --nonce "$(($POD_NONCE + 1))" |  grep -oE '0x[0-9a-fA-F]{40}')
+
+echo SOURCE_ADD_NONCE: $SOURCE_CHAIN_NONCE
+echo Addresses for nonces $SOURCE_CHAIN_NONCE to $(($SOURCE_CHAIN_NONCE + 4)):
+cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 0))" |  grep -oE '0x[0-9a-fA-F]{40}'
+cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 1))" |  grep -oE '0x[0-9a-fA-F]{40}'
+cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 2))" |  grep -oE '0x[0-9a-fA-F]{40}'
+cast compute-address "$SOURCE_CHAIN_DEPLOYER" --nonce "$(($SOURCE_CHAIN_NONCE + 3))" |  grep -oE '0x[0-9a-fA-F]{40}'
 
 echo "Precomputed:"
 echo "  Source chain BridgeDepositWithdraw -> $SOURCE_CHAIN_BRIDGE_ADDR"
@@ -77,6 +84,7 @@ echo "  Pod   BridgeMintBurn               -> $POD_BRIDGE_ADDR"
 # 3) Deploy BridgeMintBurn on Pod and configure a test token, pointing to precomputed Anvil bridge address
 forge script ./script/DeployMintBurn.s.sol:DeployMintBurn \
   --rpc-url "$POD_RPC" --private-key "$PK_POD" --broadcast --slow --skip-simulation \
+  --gas-limit 30000000 \
   --sig "run(address,(uint256,uint256,uint256),uint96,string,string,address,address,uint8)" \
   "$SOURCE_CHAIN_BRIDGE_ADDR" "($NATIVE_MIN,$NATIVE_DEP,$NATIVE_CLA)" "$SOURCE_CHAIN_ID" "$TOKEN_NAME" "$TOKEN_SYMBOL" "$POD_TOKEN_ADDR" "$SOURCE_CHAIN_TOKEN_ADDR" "$DECIMALS" \
   -vvv
@@ -92,6 +100,7 @@ forge script ./script/DeployDepositWithdraw.s.sol:DeployDepositWithdraw \
 # 5) Grant roles to BridgeMintBurn
 forge script ./script/GrantRoles.s.sol:GrantRoles \
   --rpc-url "$POD_RPC" --private-key "$PK_POD" --broadcast --slow --skip-simulation \
+  --gas-limit 30000000 \
   --sig "run(address,address)" \
   "$POD_TOKEN_ADDR" "$POD_BRIDGE_ADDR" \
   -vvv
@@ -109,18 +118,18 @@ cast send $SOURCE_CHAIN_TOKEN_ADDR "approve(address,uint256)" --rpc-url "$SOURCE
 # 8) Show codes to confirm deployment when debugging
 echo "Codes:"
 echo "  Source Chain BridgeDepositWithdraw code bytes: $(cast code $SOURCE_CHAIN_BRIDGE_ADDR --rpc-url $SOURCE_CHAIN_RPC | wc -c)"
-echo "  Pod BridgeMintBurn                 code bytes: $(cast code $POD_BRIDGE_ADDR --rpc-url $POD_RPC   | wc -c)"
 echo "  Source Chain Token                 code bytes: $(cast code $SOURCE_CHAIN_TOKEN_ADDR --rpc-url $SOURCE_CHAIN_RPC | wc -c)"
+echo "  Pod BridgeMintBurn                 code bytes: $(cast code $POD_BRIDGE_ADDR --rpc-url $POD_RPC   | wc -c)"
 echo "  Pod Mirror Token                   code bytes: $(cast code $POD_TOKEN_ADDR --rpc-url $POD_RPC   | wc -c)"
 
 # Expected values:
 #   Source Chain BridgeDepositWithdraw code bytes:    26453
-#   Pod BridgeMintBurn                 code bytes:    49063
 #   Source Chain Token                 code bytes:    13225
+#   Pod BridgeMintBurn                 code bytes:    49063
 #   Pod Mirror Token                   code bytes:    13225
 
 echo "Done."
 echo "Source Chain BridgeDepositWithdraw: $SOURCE_CHAIN_BRIDGE_ADDR"
-echo "Pod BridgeMintBurn:                 $POD_BRIDGE_ADDR"
 echo "Source Chain Token:                 $SOURCE_CHAIN_TOKEN_ADDR"
+echo "Pod BridgeMintBurn:                 $POD_BRIDGE_ADDR"
 echo "Pod Mirror Token:                   $POD_TOKEN_ADDR"
