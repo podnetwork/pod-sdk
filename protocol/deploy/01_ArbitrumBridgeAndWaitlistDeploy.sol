f6fc611858e594aa1ffdd6ec320a9ef9785c264a
Shresth Agrawal <shresthagrawal.31@gmail.com>
2026-03-30 18:30:34 +0200

    chore: add arbitrum deployment script

---
+++ protocol/deploy/01_ArbitrumBridgeAndWaitlistDeploy.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {console, Script} from "forge-std/Script.sol";
+import {Bridge} from "pod-protocol/Bridge.sol";
+import {BridgeDeployer} from "../script/DeployBridge.s.sol";
+import {DepositWaitingList} from "pod-protocol/DepositWaitingList.sol";
+
+contract DeployArbitrumBridgeAndWaitlist is Script {
+    function run() public {
+        address adminOnArbitrum = vm.envOr("ADMIN_ADDRESS", msg.sender);
+        address bridgePrecompileOnPod = address(0x50d0000000000000000000000000000000000001);
+        uint256 podChainId = 0x50d;
+        uint256 version = 0;
+
+        // The aim of this deployment is to make the waitlist work.
+        // The waitlist depends heavily on the bridge, so we are
+        // forced to deploy the bridge as well. We config the bridge
+        // the admin as the only validator, so that claims fail.
+        // In future, when we intend to make the bridge work,
+        // we should remove the admin validator and add the actual
+        // validators.
+        address[] memory initialValidators = new address[](1);
+        initialValidators[0] = adminOnArbitrum;
+        uint64 f = 0;
+        bytes32 merkleRoot = bytes32(0);
+
+        vm.startBroadcast();
+
+        (Bridge bridgeProxy, Bridge implementation) = BridgeDeployer.deploy(
+            bridgePrecompileOnPod, podChainId, adminOnArbitrum, initialValidators, f, version, merkleRoot
+        );
+
+        DepositWaitingList waitingList = new DepositWaitingList(address(bridgeProxy), adminOnArbitrum);
+
+        vm.stopBroadcast();
+
+        console.log("Bridge implementation deployed at:", address(implementation));
+        console.log("Bridge proxy deployed at:", address(bridgeProxy));
+        console.log("WaitingList deployed at:", address(waitingList));
+    }
+}
---
+++ protocol/deploy/02_ArbitrumWhitelist.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {console, Script} from "forge-std/Script.sol";
+import {Bridge} from "pod-protocol/Bridge.sol";
+import {BridgeDeployer} from "../script/DeployBridge.s.sol";
+import {DepositWaitingList} from "pod-protocol/DepositWaitingList.sol";
+
+contract ArbitrumWhitelist is Script {
+    function run() public {
+        // TODO: update with actual address after deployment
+        Bridge bridgeProxyOnArbitrum = Bridge(0xA79B109ae254A3b132866234d2bfc3D56df197e7); 
+        address usdcOnArbitrum = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+        address usdcOnPod = address(0x50D0000000000000000000000000000000000002);
+        address exchangeOnPod = address(0x50d0000000000000000000000000000000000003);
+        uint256 minAmount = 5 * 10 ** 6; // 5 USDC with 6 decimals
+        uint256 depositLimit = 1000000 * 10 ** 6; // 1 million USDC with 6 decimals
+        uint256 claimLimit = 100000 * 10 ** 6; // 100k USDC with 6 decimals
+
+        vm.startBroadcast();
+        bridgeProxyOnArbitrum.whiteListToken(usdcOnArbitrum, usdcOnPod, minAmount, depositLimit, claimLimit);
+        bridgeProxyOnArbitrum.setCallContractWhitelist(exchangeOnPod, true);
+        vm.stopBroadcast();
+    }
+}
